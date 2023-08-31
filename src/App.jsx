f8d69ed1aad931125bb8fb3f38847f719b51405c
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-31 14:02:49 +0545

    fix: added dummy content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Navbar from '../src/components/Nabar.jsx'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1>Hello World</h1>
+      <h1>Welcome to the Blog App</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Neque rem explicabo deserunt sed dignissimos.
+        Qui fugiat perspiciatis quos, at nostrum, cupiditate necessitatibus 
+        error ipsa inventore sit veritatis quasi vel sed.</p>
+      <Footer/>
     </>
   )
 }
