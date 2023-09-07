f0245731af824564fc5f2511b6bbf46f200cca71
Kushal Bajracharya <bajrakus20@gmail.com>
2023-09-07 19:20:35 +0545

    Added newletter component

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,11 @@
+const Newsletter = () => {
+    return (
+      <div>
+       <h3>Sign Up for our Newsletter</h3>
+       <input className="form-control" type="text" placeholder="Enter your email" aria-label="default input example"></input>
+       <button type="button" className="btn btn-primary">Sign Up</button>
+      </div>
+    );
+  };
+  
+  export default Newsletter;
