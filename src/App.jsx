a280608390aa90aa0198cf8d1746e140b73b2049
Kushal Bajracharya <bajrakus20@gmail.com>
2023-09-07 19:20:15 +0545

    Added newsletter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css'
 import Navbar from '../src/components/Nabar.jsx'
 import Footer from './components/Footer'
+import Newsletter from './components/Newsletter'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Newsletter />
       <h1>Welcome to the Blog App</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         Neque rem explicabo deserunt sed dignissimos.
         Qui fugiat perspiciatis quos, at nostrum, cupiditate necessitatibus 
-        error ipsa inventore sit veritatis quasi vel sed.</p>
-      <Footer/>
+        error ipsa inventore sit veritatis quasi vel sed.</p> */
+       <Footer/>
     </>
   )
 }
