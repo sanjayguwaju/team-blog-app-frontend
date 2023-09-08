60b8bdb7196d28845642c122877c14ac3c50d052
sanjayguwaju <sanjayguwaju@gmail.com>
2023-09-08 21:32:18 +0545

    fix: read more  a tag

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './Card.css';
 
 const Card = () => {
   return (
     <div>
-        <div className="card" style={{width: "20rem"}}>
-            <div className="card-body">
+        <div className="card custom-card" style={{width: "20rem"}}>
+            <div className="card-body ">
             <h5 className="card-title">About the blog</h5>
             <p className="card-text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Cumque totam, eligendi quos animi fugiat eveniet corporis 
                 veritatis perspiciatis illum. Facere accusamus iusto magni 
                 odio ratione rem ab quod sequi voluptate!
-            </p>
             <a href="#" className="btn btn-link text-decoration-none">Read More..</a>
+            </p>
             </div>
         </div>
     </div>
