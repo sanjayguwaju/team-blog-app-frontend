f1c697c20049750fe4f5425def1a367a2664c3af
Kushal Bajracharya <bajrakus20@gmail.com>
2023-09-12 21:27:57 +0545

    added popular and recent card

---
+++ src/components/PopularCard.jsx
@@ -0,0 +1,22 @@
+const PopularCard = () => {
+    return (
+        <div className="col-3">
+            <h2 className="mb-3">Popular posts</h2>
+            <div className="list-group mb-3">
+                <a href="#0" className="list-group-item list-group-item-action">
+                    <div className="badge bg-primary float-end">Category</div>
+                    <h5>Blog post title</h5>
+                    <p className="small mb-2">Author Name (<time dateTime="2017-03-12T10:24">March 12, 2017 at 10:24 AM</time>)</p>
+                    <div className="small mb-2">
+                        <span className="badge bg-primary"><i className="fa fa-thumbs-up" aria-hidden="true"></i> 13 <span className="visually-hidden">likes</span></span>
+                        <span className="badge bg-primary"><i className="fa fa-comments" aria-hidden="true"></i> 3 <span className="visually-hidden">comments</span></span>
+                    </div>
+                    <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </a>
+                {/* Repeat this block for other popular posts */}
+            </div>      
+        </div>
+    );
+}
+
+export default PopularCard;
