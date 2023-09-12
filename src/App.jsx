2e8be4cfc2e9e11d00b356aea0f142ab228dcf09
sanjayguwaju <sanjayguwaju@gmail.com>
2023-09-12 21:59:37 +0545

    feat: added layout to the blog app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import './App.css'
-import Navbar from '../src/components/Nabar.jsx'
-import Footer from './components/Footer'
+import './App.css';
+import Navbar from '../src/components/Nabar.jsx';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1>Welcome to the Blog App</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Neque rem explicabo deserunt sed dignissimos.
-        Qui fugiat perspiciatis quos, at nostrum, cupiditate necessitatibus 
-        error ipsa inventore sit veritatis quasi vel sed.</p>
-      <Footer/>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-2">
+            <p>Column 1</p>
+          </div>
+          <div className="col-md-6">
+            <p>Column 2</p>
+            <h1>Welcome to the Blog App</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque rem
+              explicabo deserunt sed dignissimos. Qui fugiat perspiciatis quos, at
+              nostrum, cupiditate necessitatibus error ipsa inventore sit veritatis
+              quasi vel sed.
+            </p>
+          </div>
+          <div className="col-md-3">
+            <p>Column 3</p>
+          </div>
+          <div className="col-md-1">
+            <p>Column 4</p>
+          </div>
+        </div>
+      </div>
+    
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
