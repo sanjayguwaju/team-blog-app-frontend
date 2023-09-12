35b0853963e73eb9b0b070a1a929dc9b84abafb9
smriteemanandhar <smriteemanandhar@gmail.com>
2023-09-12 19:52:00 +0545

    Added: Tag Cloud

--- src/components/TagCloud.jsx
+++ src/components/TagCloud.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const TagCloud = () => {
     return (
-      <div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Tag cloud</h5>
-            <a href="#" className="badge bg-secondary">#tag</a>
-            <a href="#" className="badge bg-secondary">#longtag</a>
-            <a href="#" className="badge bg-secondary">#tag</a>
-            <a href="#" className="badge bg-secondary">#longertag</a>
-            <a href="#" className="badge bg-secondary">#tag</a>
-            <a href="#" className="badge bg-secondary">#verylongtag</a>
-            <a href="#" className="badge bg-secondary">#tag</a>
-            <a href="#" className="badge bg-secondary">#longtag</a>
-            <a href="#" className="badge bg-secondary">#tag</a>
-            <a href="#" className="badge bg-secondary">#longertag</a>
-            <a href="#" className="badge bg-secondary">#tag</a>
-            <a href="#" className="badge bg-secondary">#verylongtag</a>
-            <a href="#" className="badge bg-secondary">#tag</a>
-            <a href="#" className="badge bg-secondary">#longtag</a>
-            <a href="#" className="badge bg-secondary">#tag</a>
-            <a href="#" className="badge bg-secondary">#longertag</a>
-            <a href="#" className="badge bg-secondary">#tag</a>
-            <a href="#" className="badge bg-secondary">#verylongtag</a>
-            <a href="#" className="badge bg-secondary">#tag</a>
-          </div>
+        <div>
+            <div className="card custom-card" style={{ width: "20rem" }}>
+                <div className="card-body">
+                    <h5 className="card-title">Tag cloud</h5>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#longtag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#longertag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#verylongtag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#longtag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#longertag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#verylongtag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#longtag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#longertag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#verylongtag</a>
+                    <a href="#" className="badge bg-secondary" style={{ textDecoration: 'none' }}>#tag</a>
+                </div>
+            </div>
         </div>
-      </div>
     );
-  };
+};
 
 export default TagCloud
