b016d685edc88ebc5be220fa769398c278a37bb6
smriteemanandhar <smriteemanandhar@gmail.com>
2023-09-12 17:19:07 +0545

    Deleted: import Route line

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
@@ -12,9 +11,9 @@ const Card = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Cumque totam, eligendi quos animi fugiat eveniet corporis 
                 veritatis perspiciatis illum. Facere accusamus iusto magni 
-                odio ratione rem ab quod sequi voluptate!
+                odio ratione rem ab quod sequi voluptate! 
+                <a href="#" className="btn btn-link text-decoration-none">Read More..</a>
             </p>
-            <a href="#" className="btn btn-link text-decoration-none">Read More..</a>
             </div>
         </div>
     </div>
