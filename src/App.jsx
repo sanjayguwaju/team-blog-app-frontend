58544f311e6f00136a070185a7c98272ac159991
smriteemanandhar <smriteemanandhar@gmail.com>
2023-09-12 19:17:38 +0545

    Added: TagCloud

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from '../src/components/Nabar.jsx'
 import Footer from './components/Footer'
+import TagCloud from './components/TagCloud'
 
 function App() {
   return (
     <>
       <Navbar />
+      <TagCloud />  
       <h1>Welcome to the Blog App</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         Neque rem explicabo deserunt sed dignissimos.
---
+++ src/components/TagCloud.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const TagCloud = () => {
+    return (
+      <div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Tag cloud</h5>
+            <a href="#" className="badge bg-secondary">#tag</a>
+            <a href="#" className="badge bg-secondary">#longtag</a>
+            <a href="#" className="badge bg-secondary">#tag</a>
+            <a href="#" className="badge bg-secondary">#longertag</a>
+            <a href="#" className="badge bg-secondary">#tag</a>
+            <a href="#" className="badge bg-secondary">#verylongtag</a>
+            <a href="#" className="badge bg-secondary">#tag</a>
+            <a href="#" className="badge bg-secondary">#longtag</a>
+            <a href="#" className="badge bg-secondary">#tag</a>
+            <a href="#" className="badge bg-secondary">#longertag</a>
+            <a href="#" className="badge bg-secondary">#tag</a>
+            <a href="#" className="badge bg-secondary">#verylongtag</a>
+            <a href="#" className="badge bg-secondary">#tag</a>
+            <a href="#" className="badge bg-secondary">#longtag</a>
+            <a href="#" className="badge bg-secondary">#tag</a>
+            <a href="#" className="badge bg-secondary">#longertag</a>
+            <a href="#" className="badge bg-secondary">#tag</a>
+            <a href="#" className="badge bg-secondary">#verylongtag</a>
+            <a href="#" className="badge bg-secondary">#tag</a>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export default TagCloud
