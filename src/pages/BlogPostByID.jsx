9ab6a8e607e7eeb76536c1f7188cfc90d511bae2
sanjayguwaju <sanjayguwaju@gmail.com>
2023-09-19 22:09:11 +0545

    fix: fetched data from blog post by id

--- src/pages/BlogPostByID.jsx
+++ src/pages/BlogPostByID.jsx
@@ -1,14 +1,23 @@
-import Navbar from "../components/Nabar"
+import { useState, useEffect} from "react"
+import axios from "axios"
+import { useParams } from "react-router-dom"
 import BlogPost from "../components/BlogPost"
-import Footer from "../components/Footer"
 
 const BlogPostByID = () => {
+  const [postbyId, setPostById] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`${process.env.SERVER_URL}/blogs/getblogpostbyid/${id}`)
+      .then(response => {
+        setPostById(response.data);
+      })
+  }, [id]);
+
   return (
-    <div>
-        <Navbar />
-        <BlogPost />
-        <Footer />
-    </div>
+    <>
+      <BlogPost title={postbyId.title} content={postbyId.content} image={postbyId.image}/>
+    </>
   )
 }
 
