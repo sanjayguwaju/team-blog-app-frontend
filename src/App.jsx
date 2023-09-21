ad5c22225f1bd15a9648aadc60c36f8e8650eeea
sanjayguwaju <sanjayguwaju@gmail.com>
2023-09-21 09:15:14 +0545

    fix: added single blog post layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Routes, Route} from 'react-router-dom'
 import Homepage from './pages/Homepage';
 import BlogPostByID from './pages/BlogPostByID';
 import MainLayout from './components/MainLayout';
+import SingleBlogPostLayout from './components/SingleBlogPostLayout';
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<MainLayout />}>
             <Route index element={<Homepage />} />
-            <Route path="/getpostbyid/:id" element={<BlogPostByID />} />
         </Route>
-        
+        <Route path="/getpostbyid/:id" element={<SingleBlogPostLayout/>} >
+          <Route index element={<BlogPostByID />} />
+        </Route>
       </Routes>
   );
 }
