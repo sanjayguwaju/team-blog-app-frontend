f9efbc8e476e4b90b7487a6373180c11acce3aac
sanjayguwaju <sanjayguwaju@gmail.com>
2023-09-21 09:20:58 +0545

    fixed: popular card and recent card in homepage sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,24 @@
 import Card from "./Card";
 import Categories from "./Categories";
 import Newsletter from "./Newsletter";
+import RecentCard from "./RecentCard";
+import PopularCard from "./PopularCard";
 import RecentPostWidget from "./RecentPostWidget";
 import TagCloud from "./TagCloud";
 const Sidebar = () => {
   return (
     <>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       <Categories />
       <br />
       <TagCloud />
+      <br />
+      <RecentCard/>
+      <br />
+      <PopularCard/>
       <br />
       <Card />
       <br />
