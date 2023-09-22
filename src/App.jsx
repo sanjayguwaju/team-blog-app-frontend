52fdad4137b74d96f6bcc48c856b482efd0b0ac2
Kushal Bajracharya <bajrakus20@gmail.com>
2023-09-22 21:06:34 +0545

    added author name

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route} from 'react-router-dom'
 import Homepage from './pages/Homepage';
 import BlogPostByID from './pages/BlogPostByID';
 import MainLayout from './components/MainLayout';
+import Card from './components/Card';
 
 function App() {
   return (
@@ -9,8 +10,7 @@ function App() {
         <Route path="/" element={<MainLayout />}>
             <Route index element={<Homepage />} />
             <Route path="/getpostbyid/:id" element={<BlogPostByID />} />
-        </Route>
-        
+        </Route>  
       </Routes>
   );
 }
