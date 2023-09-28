f1ff92d3506bf57897b498bf80a52a271daf6fc3
Kushal Bajracharya <bajrakus20@gmail.com>
2023-09-28 20:49:57 +0545

    fixed code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,9 @@
-
+import React, { useState } from 'react';
 import { faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./Login.css";
 import axios from "axios";
-import {Link} from "react-router-dom";
-import { useState } from 'react';   
+import { Link } from "react-router-dom";
 
 const Login = () => {
     const [loginStatus, setLoginStatus] = useState(null);
@@ -14,16 +13,18 @@ const Login = () => {
         const formData = new FormData(event.target);
         const data = Object.fromEntries(formData.entries());
         axios
-        .post(`${process.env.SERVER_URL}/users/login`, data)
-        .then(response => {
-            if(response){  
-                setLoginStatus(true);
-        }
-        })
-        .catch(error => {
-            console.log(error); 
-            setLoginStatus(false);
-        });
+            .post(`${process.env.SERVER_URL}/users/login`, data)
+            .then(response => {
+                if (response) {  
+                    setLoginStatus(true);
+                }
+            })
+            .catch(error => {
+                console.log(error); 
+                setLoginStatus(false);
+            });
+    };
+
     return (
         <>
             <section className="vh-100">
@@ -33,98 +34,86 @@ const Login = () => {
                             <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid" alt="Sample image" />
                         </div>
                         <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                                <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                                    <p className="lead fw-normal mb-0 me-3">Sign in with</p>
-                                    <button type="button" className="btn btn-primary btn-floating mx-1">
-                                        <FontAwesomeIcon icon={faFacebook} />
-                                    </button>
-
-                                    <button type="button" className="btn btn-primary btn-floating mx-1">
-                                        <FontAwesomeIcon icon={faTwitter} />
-                                    </button>
-
-                                    <button type="button" className="btn btn-primary btn-floating mx-1">
+                            <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                                <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+                                <button type="button" className="btn btn-primary btn-floating mx-1">
+                                    <FontAwesomeIcon icon={faFacebook} />
+                                </button>
+                                <button type="button" className="btn btn-primary btn-floating mx-1">
+                                    <FontAwesomeIcon icon={faTwitter} />
+                                </button>
+                                <button type="button" className="btn btn-primary btn-floating mx-1">
                                     <FontAwesomeIcon icon={faLinkedin} />
-                                    </button>
-                                </div>
-
-                                <div className="divider d-flex align-items-center my-4">
-                                    <p className="text-center fw-bold mx-3 mb-0">Or</p>
-                                </div>
-                                <form 
-                                    onSubmit={handleSubmit}>
-                                {/* <!-- Email input --> */}
+                                </button>
+                            </div>
+                            <div className="divider d-flex align-items-center my-4">
+                                <p className="text-center fw-bold mx-3 mb-0">Or</p>
+                            </div>
+                            <form onSubmit={handleSubmit}>
+                                {/* Email input */}
                                 <div className="d-flex flex-row align-items-center mb-4">
                                     <label className="form-label" htmlFor="form3Example3">Email address</label>
                                     <input 
                                         name="email"    
                                         type="email" 
-                                        id="form3Example3" 
+                                        id="form1" 
                                         className="form-control form-control-lg"
                                         placeholder="Enter a valid email address" />
                                 </div>
 
-                                {/* <!-- Password input --> */}
+                                {/* Password input */}
                                 <div className="d-flex flex-row align-items-center mb-4">
                                     <label className="form-label" htmlFor="form3Example4">Password</label>
                                     <input 
                                         name="password"
                                         type="password"
-                                        id="form3Example4" 
+                                        id="form2" 
                                         className="form-control form-control-lg"
                                         placeholder="Enter password" />
-                                        
                                 </div>
 
                                 <div className="d-flex flex-row align-items-center mb-4">
-                                    {/* <!-- Checkbox --> */}
+                                    {/* Checkbox */}
                                     <div className="form-check mb-0">
                                         <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
                                         <label className="form-check-label" htmlFor="form2Example3">
                                             Remember me
                                         </label>
                                     </div>
-                                    </div>
-                                </form>  
-                                    <a href="#!" className="text-body">Forgot password?</a>
                                 </div>
-
+                                <a href="#!" className="text-body">Forgot password?</a>
                                 <div className="d-flex flex-row align-items-center mb-4">
-                                    <button type="button" className="btn btn-primary btn-lg"
-                                        style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}>Login</button>
-                                    <p className="small fw-bold mt-2 pt-1 mb-0">Dont have an account? <a href="#!"
-                                        className="link-danger">Register</a></p>
+                                    <button type="submit" className="btn btn-primary btn-lg" style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}>Login</button>
+                                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <Link to="/register" className="link-danger">Register</Link></p>
                                 </div>
+                            </form>
                         </div>
                     </div>
-                <div
-                    className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-                    {/* Copyright  */}
-                    <div className="text-white mb-3 mb-md-0 text-center">
-                        Copyright © 2020. All rights reserved.
+                    <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+                        {/* Copyright */}
+                        <div className="text-white mb-3 mb-md-0 text-center">
+                            Copyright © 2020. All rights reserved.
+                        </div>
+                        {/* Right */}
+                        <div>
+                            <a href="#!" className="text-white me-4">
+                                <i className="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#!" className="text-white me-4">
+                                <i className="fab fa-twitter"></i>
+                            </a>
+                            <a href="#!" className="text-white me-4">
+                                <i className="fab fa-google"></i>
+                            </a>
+                            <a href="#!" className="text-white">
+                                <i className="fab fa-linkedin-in"></i>
+                            </a>
+                        </div>
                     </div>
-                    {/* Copyright */}
-
-                    {/* Right */}
-                    <div>
-                        <a href="#!" className="text-white me-4">
-                            <i className="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#!" className="text-white me-4">
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                        <a href="#!" className="text-white me-4">
-                            <i className="fab fa-google"></i>
-                        </a>
-                        <a href="#!" className="text-white">
-                            <i className="fab fa-linkedin-in"></i>
-                        </a>
-                    </div>
-                    {/* <!-- Right --> */}
                 </div>
             </section>
         </>
     );
 }
-}
+
 export default Login;
