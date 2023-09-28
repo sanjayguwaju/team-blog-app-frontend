3768db5c1047936a3ce6a2f8a79e63c13bdb0405
Kushal Bajracharya <bajrakus20@gmail.com>
2023-09-28 20:33:35 +0545

    buggy commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,28 @@
 import { faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./Login.css";
+import axios from "axios";
+import {Link} from "react-router-dom";
+import { useState } from 'react';   
+
 const Login = () => {
+    const [loginStatus, setLoginStatus] = useState(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+        axios
+        .post(`${process.env.SERVER_URL}/users/login`, data)
+        .then(response => {
+            if(response){  
+                setLoginStatus(true);
+        }
+        })
+        .catch(error => {
+            console.log(error); 
+            setLoginStatus(false);
+        });
     return (
         <>
             <section className="vh-100">
@@ -12,7 +33,6 @@ const Login = () => {
                             <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid" alt="Sample image" />
                         </div>
                         <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                            <form>
                                 <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
                                     <p className="lead fw-normal mb-0 me-3">Sign in with</p>
                                     <button type="button" className="btn btn-primary btn-floating mx-1">
@@ -31,22 +51,32 @@ const Login = () => {
                                 <div className="divider d-flex align-items-center my-4">
                                     <p className="text-center fw-bold mx-3 mb-0">Or</p>
                                 </div>
-
+                                <form 
+                                    onSubmit={handleSubmit}>
                                 {/* <!-- Email input --> */}
-                                <div className="form-outline mb-4">
+                                <div className="d-flex flex-row align-items-center mb-4">
                                     <label className="form-label" htmlFor="form3Example3">Email address</label>
-                                    <input type="email" id="form3Example3" className="form-control form-control-lg"
+                                    <input 
+                                        name="email"    
+                                        type="email" 
+                                        id="form3Example3" 
+                                        className="form-control form-control-lg"
                                         placeholder="Enter a valid email address" />
                                 </div>
 
                                 {/* <!-- Password input --> */}
-                                <div className="form-outline mb-3">
+                                <div className="d-flex flex-row align-items-center mb-4">
                                     <label className="form-label" htmlFor="form3Example4">Password</label>
-                                    <input type="password" id="form3Example4" className="form-control form-control-lg"
+                                    <input 
+                                        name="password"
+                                        type="password"
+                                        id="form3Example4" 
+                                        className="form-control form-control-lg"
                                         placeholder="Enter password" />
+                                        
                                 </div>
 
-                                <div className="d-flex justify-content-between align-items-center">
+                                <div className="d-flex flex-row align-items-center mb-4">
                                     {/* <!-- Checkbox --> */}
                                     <div className="form-check mb-0">
                                         <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
@@ -54,20 +84,19 @@ const Login = () => {
                                             Remember me
                                         </label>
                                     </div>
+                                    </div>
+                                </form>  
                                     <a href="#!" className="text-body">Forgot password?</a>
                                 </div>
 
-                                <div className="text-center text-lg-start mt-4 pt-2">
+                                <div className="d-flex flex-row align-items-center mb-4">
                                     <button type="button" className="btn btn-primary btn-lg"
                                         style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}>Login</button>
                                     <p className="small fw-bold mt-2 pt-1 mb-0">Dont have an account? <a href="#!"
                                         className="link-danger">Register</a></p>
                                 </div>
-
-                            </form>
                         </div>
                     </div>
-                </div>
                 <div
                     className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
                     {/* Copyright  */}
@@ -95,7 +124,7 @@ const Login = () => {
                 </div>
             </section>
         </>
-    )
+    );
 }
-
-export default Login
+}
+export default Login;
