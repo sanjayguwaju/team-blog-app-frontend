cc09ce8fa88ed3cb87fd6d5a080b5a3c553456b0
sanjayguwaju <sanjayguwaju@gmail.com>
2023-11-13 23:37:54 +0545

    fix: navbar customization

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,58 +5,60 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
-      <div className="container">
-        <Link to={`/`} className="navbar-brand"><strong>Blogsphere Nepal</strong></Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <a href="index.html" className="nav-link">Home</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a href="#0" className="nav-link dropdown-toggle active" id="blogDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Blog<span className="visually-hidden">(current)</span>
-              </a>
-              <div className="dropdown-menu" aria-labelledby="blogDropdown">
-                <a href="index.html" className="dropdown-item active">List</a>
-                <a href="blog-overview-grid.html" className="dropdown-item">Grid</a>
-              </div>
-            </li>
-            <li className="nav-item dropdown">
-              <a href="#0" className="nav-link dropdown-toggle" id="archiveDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Archive</a>
-              <div className="dropdown-menu" aria-labelledby="archiveDropdown">
-                <h6 className="dropdown-header">2017</h6>
-                <div className="dropdown-divider"></div>
-                <h6 className="dropdown-header">2016</h6>``
-              </div>
-            </li>
-            <li className="nav-item">
-              <a href="#0" className="nav-link">About</a>
-            </li>
-          </ul>
-          <form className="d-flex">
+    <>
+      <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
+        <div className="container">
+          <Link to={`/`} className="navbar-brand"><strong>Blogsphere Nepal</strong></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a href="index.html" className="nav-link">Home</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a href="#0" className="nav-link dropdown-toggle active" id="blogDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Blog<span className="visually-hidden">(current)</span>
+                </a>
+                <div className="dropdown-menu" aria-labelledby="blogDropdown">
+                  <a href="index.html" className="dropdown-item active">List</a>
+                  <a href="blog-overview-grid.html" className="dropdown-item">Grid</a>
+                </div>
+              </li>
+              <li className="nav-item dropdown">
+                <a href="#0" className="nav-link dropdown-toggle" id="archiveDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Archive</a>
+                <div className="dropdown-menu" aria-labelledby="archiveDropdown">
+                  <h6 className="dropdown-header">2017</h6>
+                  <div className="dropdown-divider"></div>
+                  <h6 className="dropdown-header">2016</h6>``
+                </div>
+              </li>
+              <li className="nav-item">
+                <a href="#0" className="nav-link">About</a>
+              </li>
+            </ul>
+            <form className="d-none d-md-none d-lg-flex">
             <input type="text" name="search" className="form-control me-2" placeholder="Search" aria-label="Search" />
             <button type="submit" className="btn btn-primary">
               <FontAwesomeIcon icon={faSearch} aria-hidden="true" />
               <span className="visually-hidden">Search</span>
             </button>
           </form>
-
-          <div className="d-flex">
-            <div className="mx-3 d-flex">
-              <Link to="/write" className="mx-3">
-                <button className="btn btn-primary">
-                  <FontAwesomeIcon
-                    icon={faPenToSquare}
-                    aria-hidden="true"
-                    style={{ color: "white" }}
-                  />
-                </button>
-              </Link>
-              <div className="mx-3 dropdown">
+          
+            <div className='button__container d-flex justify-content-between'>
+              <div className="button_container_left">
+                <Link to="/write" className="mx-3">
+                  <button className="btn btn-primary">
+                    <FontAwesomeIcon
+                      icon={faPenToSquare}
+                      aria-hidden="true"
+                      style={{ color: "white" }}
+                    />
+                  </button>
+                </Link>
+              </div>
+              <div className="button_container_right">
                 <button
                   className="btn btn-primary dropdown-toggle"
                   type="button"
@@ -89,30 +91,39 @@ const Navbar = () => {
                 </ul>
               </div>
             </div>
-            <Link to="/register" className="mx-3">
-              <button className="btn btn-primary">
-                Register
-              </button>
-            </Link>
-            <Link to="/login" className="mx-3">
-              <button className="btn btn-primary">
-                Login
-              </button>
-            </Link>
+            <div className="d-flex justify-content-between">
+              <Link to="/register" className="mx-3">
+                <button className="btn btn-primary">
+                  Register
+                </button>
+              </Link>
+              <Link to="/login" className="mx-3">
+                <button className="btn btn-primary">
+                  Login
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* <form className="d-flex">
-      <input
-      type="text"
-      name="search"
-      className="form-control full-width-input me-2"
-      placeholder="Search"
-      aria-label="Search" />
-        <button type="submit" className="btn btn-primary"><FontAwesomeIcon icon={faSearch} aria-hidden="true" /><span className="visually-hidden">Search</span></button>
-      </form> */}
-    </nav>
+      </nav>
+      <div className='mx-2 my-2 d-block d-md-block d-lg-none'>
+        <form className="d-flex">
+          <input
+            type="text"
+            name="search"
+            className="form-control full-width-input me-2"
+            placeholder="Search"
+            aria-label="Search" />
+          <button type="submit" className="btn btn-primary">
+            <FontAwesomeIcon icon={faSearch} aria-hidden="true" />
+            <span className="visually-hidden">
+              Search
+            </span>
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
