48fadfee33854b8dfaedd8f8b379b401ffc086de
sanjayguwaju <sanjayguwaju@gmail.com>
2023-11-27 10:43:33 +0545

    fix: code refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom';
 
+// --- JSX imports ------
+import App from './App.jsx'
+
 // ----- CSS for the packages -----
+import './index.css'
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-quill/dist/quill.snow.css";
