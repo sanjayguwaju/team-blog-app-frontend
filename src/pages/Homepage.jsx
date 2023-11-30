4e9dbc9ff58754ad1b1f8950c8d889acdb97417f
Kushal Bajracharya <bajrakus20@gmail.com>
2023-11-30 21:43:21 +0545

    added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,38 @@ import { useEffect, useState } from "react";
 import Pagination from "../components/Pagination";
 import axios from "axios";
 import BlogPost from "../components/BlogPost";
+import ReactPaginate from "react-paginate";
 
 const Homepage = () => {
     const [posts, setPost] = useState([]);
     const [showTrimmedPost, setshowTrimmedPost] = useState(true);
     const [ShowReadMore, setShowReadMore] = useState(false);
     const [showCommentSection, setShowCommentSection] = useState(false);
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const postPerPage = 2; 
+    const pagesVisited = pageNumber * postPerPage;
+
+    const displayPosts = posts.slice(pagesVisited, pagesVisited + postPerPage)
+    .map((postdata) => (
+      <BlogPost
+        key={postdata._id}
+        id={postdata._id}
+        title={postdata.title}
+        content={postdata.content}
+        image={postdata.image}
+        createdAt={postdata.createdAt}
+        ShowReadMore={ShowReadMore}
+        author={
+          postdata?.author?.name
+            ? postdata?.author?.name
+            : "no author is avaliable"
+        }
+        showTrimmedPost={showTrimmedPost}
+        showCommentSection={showCommentSection}
+      />
+    ));
+
   useEffect(() => {
     if (posts) {
       setShowReadMore(true);
@@ -33,27 +59,26 @@ const Homepage = () => {
       });
   }
 
+  const pageCount = Math.ceil(posts.length / postPerPage);
+
+  const pageChange = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <>
-      {posts.map((postdata) => (
-        <BlogPost
-          key={postdata._id}
-          id={postdata._id}
-          title={postdata.title}
-          content={postdata.content}
-          image={postdata.image}
-          createdAt={postdata.createdAt}
-          ShowReadMore={ShowReadMore}
-          author={
-            postdata?.author?.name
-              ? postdata?.author?.name
-              : "no author is avaliable"
-          }
-          showTrimmedPost={showTrimmedPost}
-          showCommentSection={showCommentSection}
+      {displayPosts}
+      <ReactPaginate
+      previousLabel={"Previous"}
+      nextLabel={"Next"}
+      pageCount={pageCount}
+      onPageActive={pageChange}
+      containerClassName={"paginationBtn"}
+      previousLinkClassName={"previousBtn"}
+      nextLinkClassName={"nextBtn"}
+      disabledClassName={"paginationDisabled"}
+      activeClassName={"paginationActive"}
         />
-      ))}
-      <Pagination />
     </>
   );
 };
