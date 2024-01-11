eab9dcca1bdb8057fabe423f8ebb257880a2e7c3
Kushal Bajracharya <bajrakus20@gmail.com>
2024-01-11 19:24:53 +0545

    Remove the underline form footer and added react-router-dom for Home

--- src/components/modules/footer/Footer.jsx
+++ src/components/modules/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faTwitter, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -9,31 +10,31 @@ const Footer = () => {
           <div className="col-12 col-sm-6 col-md-3">
             <h6>Quick links</h6>
             <ul className="list-unstyled">
-              <li><a href="#0">Home</a></li>
-              <li><a href="#0">Archive</a></li>
-              <li><a href="#0">Categories</a></li>
-              <li><a href="#0">Tag cloud</a></li>
-              <li><a href="#0">Search</a></li>
+              <li><a href="/" style={{ textDecoration: 'none' }}>Home</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}>Archive</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}>Categories</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}>Tag cloud</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}>Search</a></li>
             </ul>
           </div>
           <div className="col-12 col-sm-6 col-md-3">
             <h6>Information</h6>
             <ul className="list-unstyled">
-              <li><a href="#0">About us</a></li>
-              <li><a href="#0">Authors</a></li>
-              <li><a href="#0">Newsletter</a></li>
-              <li><a href="#0">Advertising</a></li>
-              <li><a href="#0">Sitemap</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}>About us</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}>Authors</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}>Newsletter</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}>Advertising</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}>Sitemap</a></li>
             </ul>
           </div>
           <div className="col-12 col-sm-6 col-md-3">
             <h6>Follow us</h6>
             <ul className="list-unstyled">
-              <li><a href="#0"><FontAwesomeIcon icon={faFacebook} /> Facebook</a></li>
-              <li><a href="#0"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
-              <li><a href="#0"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
-              <li><a href="#0"><FontAwesomeIcon icon={faYoutube} /> YouTube</a></li>
-              <li><a href="#0"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faFacebook} /> Facebook</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faYoutube} /> YouTube</a></li>
+              <li><a href="#0" style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</a></li>
             </ul>
           </div>
           <div className="col-12 col-sm-6 col-md-3">
@@ -42,15 +43,15 @@ const Footer = () => {
               <strong>Blog Name</strong><br />
               Street name<br />
               ZIP code and city<br />
-              <abbr title="Telephone">T:</abbr> <a href="tel:+1234567890">(123) 456-7890</a><br />
-              <abbr title="Mail">M:</abbr> <a href="mailto:info@domain.com">info@domain.com</a>
+              <abbr title="Telephone">T:</abbr> <a href="tel:+1234567890" style={{ textDecoration: 'none' }}>(123) 456-7890</a><br />
+              <abbr title="Mail">M:</abbr> <a href="mailto:info@domain.com" style={{ textDecoration: 'none' }}>info@domain.com</a>
             </address>
           </div>
         </div>
         <ul className="list-inline text-center">
           <li className="list-inline-item">&copy; {new Date().getFullYear()} Blogsphere Nepal</li>
           <li className="list-inline-item">All rights reserved.</li>
-          <li className="list-inline-item"><a href="#0">Terms of use and privacy policy</a>.</li>
+          <li className="list-inline-item"><a href="#0" style={{ textDecoration: 'none' }}>Terms of use and privacy policy</a>.</li>
         </ul>
       </div>
     </footer>
